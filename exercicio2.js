a5cbb0c663a5d60c4107a84ed25bcdfbc198b322
NAOMI SALVADOR <04682461001@senacrs.edu.br>
2024-09-10 17:32:29 -0300

    exercicio 02

---
+++ exercicio2.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout
+});
+
+console.log("Boas, Vindas")
+rl.question('escreva seu nome: ', (nome) => {
+    console.log(`Olá, ${nome}! `)
+    rl.close();
+})
