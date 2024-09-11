480f432ae5b3987019407a953188c62ee5cb2f75
NAOMI SALVADOR <04682461001@senacrs.edu.br>
2024-09-11 17:21:15 -0300

    https://github.com/itsnaomii/front-end-s.git

---
+++ aula 3/3exercicio1.js
@@ -0,0 +1,15 @@
+let graos = ['arroz', 'feijão', 'lentilha']
+
+/* const adicionarFimGraos = () => {
+    graos.push('ervilha')
+    console.log(graos)
+}
+
+adicionarFimGraos() */
+
+const adicionarFimGraos = (grao) => {
+    graos.push(grao)
+    console.log(graos)
+}
+
+adicionarFimGraos('ervilha')
---
+++ aula 3/3exercicio2.js
@@ -0,0 +1,8 @@
+let graos = ['arroz', 'feijão', 'lentilha']
+
+const removerInicioGraos = (graos) => {
+    graos.shift()
+    console.log(graos)
+}
+
+removerInicioGraos('arroz')
